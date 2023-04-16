a236cbd70e847e8c8ea63c408a479bdb76be8764
AntonioValleD <juan.valle.d1@gmail.com>
2023-04-16 09:09:01 -0600

    Error messages updated

--- src/auth/auth.handler.js
+++ src/auth/auth.handler.js
@@ -6,21 +6,30 @@ const usersController = require('./users.controller');
 const userSignUp = async (req, res) => {
     if (!req.body.userName || !req.body.email || !req.body.password){
         // No Sign Up data was provided inside the body
-        return res.status(400).json({message: 'No data was provided'});
+        return res.status(400).json({
+            message: 'No data was provided',
+            error: 'User name, email and password is needed to create an account'
+        });
     }
 
     try {
         await usersController.registerUser(req.body.userName, req.body.email, req.body.password);
         return res.status(200).json({message: 'Sign up successfully'});
     } catch (error) {
-        return res.status(405).json({message: 'User already exists'});
+        return res.status(405).json({
+            message: 'User already exists',
+            error: 'User email is already registered, please use other email account'
+        });
     }
 };
 
 const userLogin = async (req, res) => {
     if (!req.body.email || !req.body.password){
         // No login data was provided inside the body
-        return res.status(400).json({message: 'No data was provided'});
+        return res.status(400).json({
+            message: 'No data was provided',
+            error: 'Email and password is needed to login'
+        });
     }
 
     try {
--- src/task/task.handler.js
+++ src/task/task.handler.js
@@ -3,7 +3,10 @@ const taskController = require('./task.controller');
 
 const getTaskList = async (req, res) => {
     if (!req.body.filter){
-        return res.status(400).json({message: 'Missing data'});
+        return res.status(400).json({
+            message: 'Missing data',
+            error: 'Filter object (all/completed/missing) is needed'
+        });
     }
     try {
         let taskList = await taskController.getUserTasks(req.user.userId, req.body.filter);
@@ -15,49 +18,73 @@ const getTaskList = async (req, res) => {
 
 const addUserTask = async (req, res) => {
     if (!req.body.taskBody){
-        return res.status(400).json({message: 'Missing data'});
+        return res.status(400).json({
+            message: 'Missing data',
+            error: 'TaskBody object (string) is needed'
+        });
     }
     try {
         await taskController.addTask(req.user.userId, req.body.taskBody);
         return res.status(201).json({message: 'Task created successfully'});
     } catch (error) {
-        return res.status(400).json({message: 'Missing data'});
+        return res.status(400).json({
+            message: 'Missing data',
+            error: 'TaskBody must not be an empty object'
+        });
     }
 };
 
 const setUpUserTasks = async (req, res) => {
     if (!req.body.taskList){
-        return res.status(400).json({message: 'Missing data'});
+        return res.status(400).json({
+            message: 'Missing data',
+            error: 'TaskList object (array) is needed'
+        });
     }
     try {
         await taskController.setUpTasks(req.user.userId, req.body.taskList);
         return res.status(200).json({message: 'Tasks created successfully'});
     } catch (error) {
-        return res.status(400).json({message: 'Missing data'});
+        return res.status(400).json({
+            message: 'Missing data',
+            error: 'TaskList must not be an empty object'
+        });
     }
 };
 
 const editUserTask = async (req, res) => {
     if (req.body.status){
         if (!req.body.index && req.body.index != 0){
-            return res.status(400).json({message: 'Missing data'});
+            return res.status(400).json({
+                message: 'Missing data',
+                error: 'Task index is needed'
+            });
         }
         try {
             await taskController.changeTaskStatus(req.user.userId, req.body.index);
             return res.status(200).json({message: 'Tasks status updated successfully'});
         } catch (error) {
             console.log(error);
-            return res.status(400).json({message: 'Task not found'});
+            return res.status(400).json({
+                message: 'Task not found',
+                error: 'Provided task index does not exist in database'
+            });
         }
     } else {
         if (!req.body.taskBody || (!req.body.index && req.body.index != 0)){
-            return res.status(400).json({message: 'Missing data'});
+            return res.status(400).json({
+                message: 'Missing data',
+                error: 'taskBody and index objects are needed'
+            });
         }
         try {
             await taskController.editTaskBody(req.user.userId, req.body.index, req.body.taskBody);
             return res.status(200).json({message: 'Task edited successfully'});
         } catch (error) {
-            return res.status(400).json({message: 'Task not found'});
+            return res.status(400).json({
+                message: 'Task not found',
+                error: 'Provided task index does not exist in database'
+            });
         }
     }
 };
@@ -68,13 +95,19 @@ const deleteUserTask = async (req, res) => {
         res.status(200).json({message: 'All tasks deleted successfully'});
     } else {
         if (!req.body.index && req.body.index != 0){
-            return res.status(400).json({message: 'Missing data'});
+            return res.status(400).json({
+                message: 'Missing data',
+                error: 'Task index is needed'
+            });
         }
         try {
             await taskController.deleteTask(req.user.userId, req.body.index);
             return res.status(200).json({message: 'Task deleted successfully'});
         } catch (error) {
-            return res.status(400).json({message: 'Task not found'});
+            return res.status(400).json({
+                message: 'Task not found',
+                error: 'Provided task index does not exist in database'
+            });
         }
     }
 };
