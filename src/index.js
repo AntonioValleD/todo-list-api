9ce1505af7b080a6840ce2164b2128e0fcaca97d
AntonioValleD <juan.valle.d1@gmail.com>
2023-04-16 08:15:07 -0600

    env variable added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const app =  require('./app').app;
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 // Server
 app.listen(port, () => {
-    console.log('Server started at port 3000');
+    console.log(`Server started at port ${port}`);
 });
