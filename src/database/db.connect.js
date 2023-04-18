1b20c3a2006302adad37224569f1e7ce37e27af8
AntonioValleD <juan.valle.d1@gmail.com>
2023-04-17 20:19:29 -0600

    MongoDB URL log deleted

--- src/database/db.connect.js
+++ src/database/db.connect.js
@@ -1,31 +1,3 @@
-/*
-let MongoClient = require("mongodb").MongoClient;
-require('dotenv').config();
-
-//Database credentials
-// let dbUserName = 'taskUser';
-// let dbPassword = 'Dwc4MrV7KCpzS6ds';
-let url = process.env.MONGODB_URL;
-
-let dbName = 'taskManager';
-if (process.env.NODE_ENV === 'test'){
-    dbName = 'testDb';
-}
-
-// Database connection
-let client = new MongoClient(url);
-let database = client.db(dbName);
-console.log('Database connected'); 
-
-
-
-// let url = 'mongodb+srv://taskUser:Dwc4MrV7KCpzS6ds@cluster0.bqariom.mongodb.net/?retryWrites=true&w=majority'
-// let id = '68b03160-da36-11ed-926b-0361a1a22855'
-// let tasks = tasksCollection.find({userId: id});
-// tasks.forEach((doc) => {
-//     console.log(doc);
-// })
-*/
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -36,7 +8,6 @@ if (process.env.NODE_ENV === 'test'){
 
 const dbConnect = async () => {
     try {
-        console.log(url);
         await mongoose.connect(url);
         console.log('MongoDB connected...');
         /*
