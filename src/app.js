66ad1cfc4fbfa3ee2dc2258f1ba0e657a066e142
AntonioValleD <juan.valle.d1@gmail.com>
2023-04-19 17:54:26 -0600

    GET response changed from .send to .json

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ let port = process.env.PORT || 3000;
 
 // Routes
 app.get('/', (req, res) => {
-    res.status(201).send(`Server response at port ${port}`);
+    res.status(200).json(`Server response at port ${port}`);
 });
 
 app.post('/auth/signup', usersHandler.userSignUp);
